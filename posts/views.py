510180bf9dd1964322518b7ed508c18f29d8792a
Nicholas Trimble <nicktrimble07@gmail.com>
2026-01-03 15:00:50 -0600

    fixed the post pages

--- posts/views.py
+++ posts/views.py
@@ -7,3 +7,9 @@ def posts_list(request):
     posts = Post.objects.all().order_by('-date')
     return render(request, 'posts/posts_list.html', {'posts': posts})
 
+def post_page(request, slug):
+    try:
+        post = Post.objects.get(slug=slug)
+        return render(request, 'posts/post_page.html', {'post': post})
+    except Post.DoesNotExist:
+        return render(request, 'posts/post_page.html', {'post': None})
